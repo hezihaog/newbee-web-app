9e22c4df865cc84c045f61be73b668c813e5181e
zihe <he100zihao100@126.com>
2021-01-24 20:37:48 +0800

    init: 项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Button } from 'vant'
+import 'lib-flexible/flexible'
+
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>你好</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style lang="less" scoped>
+.home {
+  div {
+    background: greenyellow;
+    width: 75px;
+    height: 75px;
+    font-size: 18PX;
+  }
+}
+</style>
